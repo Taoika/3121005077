db56fb5ee70c7b082b4a7c8fb9fa1ce22c9dbe83
kkt <670335957@qq.com>
2023-09-14 22:14:01 +0800

    add: 内容清洗

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import jieba
 from bs4 import BeautifulSoup
-
+import re
 
 # 分词
 def Participle(text):
@@ -15,7 +15,15 @@ def ReadFile(path):
 
 # 清晰特殊字符 标点符号
 def Clean(phraseArr):
-    print(phraseArr)
+    # 去除特殊字符和标点符号的正则表达式
+    pattern = r'[\n\s、，。！？：；“”‘’【】『』【】（）【】《》<>"\',.!?;:()【】【】]'
+    # 使用正则表达式替换特殊字符和标点符号为空字符串
+    filtered_list = [re.sub(pattern, '', element) for element in phraseArr]
+
+    # 去除空字符串元素
+    filtered_list = [element for element in filtered_list if element]
+
+    return filtered_list
 
 # 从html中提取文本
 def ExtractionText(html):
@@ -28,10 +36,11 @@ def ExtractionText(html):
     for td in td_tags:
         textList.append(td.text)
 
-    return textList
+    string = ''.join(textList)
+    return string
 
 # 判断文本是否html代码
-def isHtml(text):
+def IsHtml(text):
     soup = BeautifulSoup(text, 'html.parser')
     td_tags = soup.find_all('td')
     if(len(td_tags)):
@@ -45,12 +54,15 @@ def main():
     path2 = 'D:/资料/学习资料/计算机/软件工程/2023/论文查重程序测试样例/orig_0.8_del.txt'
     path3 = 'D:/资料/学习资料/计算机/软件工程/2023/论文查重程序测试样例/orig_0.8_add.txt'
 
-    original_text1 = ReadFile(path1)
-    original_text2 = ReadFile(path2)
-    original_text3 = ReadFile(path3)
+    original_text1 = ReadFile(path1) # 原文
+    original_text2 = ReadFile(path2) # html
+    original_text3 = ReadFile(path3) # 抄袭文
 
+    text1 = Participle(original_text1)
+    text2 = Participle(ExtractionText(original_text2))
+    text3 = Participle(original_text3)
 
-    print(isHtml(original_text2))
+    print(Clean(text2))
 
 if __name__ == "__main__":
     main()
