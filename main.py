1c049b8af03451e94ec64dea9755bac54d714ea2
kkt <670335957@qq.com>
2023-09-14 20:52:09 +0800

    add: 简单分词模块

--- main.py
+++ main.py
@@ -5,22 +5,23 @@ import sys
 # 分词
 def Participle(text):
     seg_list = jieba.lcut(text)  # 默认是精确模式
-    print(seg_list)
+    return(seg_list)
 
-# 内容读取模块
-def read_file(path):
+# 文件读取
+def ReadFile(path):
     with open(path, 'r', encoding='utf-8') as file:
         text = file.read()
-
     return text
 
-
+# 主函数
 def main():
     path1 = 'D:/资料/学习资料/计算机/软件工程/2023/论文查重程序测试样例/orig.txt'
     path2 = 'D:/资料/学习资料/计算机/软件工程/2023/论文查重程序测试样例/orig_0.8_add.txt'
 
-    text1 = read_file(path1)
-    text2 = read_file(path2)
+    text1 = ReadFile(path1)
+    text2 = ReadFile(path2)
+
+    print(Participle(text1))
 
 
 if __name__ == "__main__":
