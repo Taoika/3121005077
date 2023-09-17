cb087c871de87248420fbbe37b613d14ca476888
kkt <670335957@qq.com>
2023-09-17 13:01:38 +0800

    add: 添加pylint作为代码质量分析工具 并消除所有警告报错

--- main.py
+++ main.py
@@ -5,77 +5,8 @@ from gensim import corpora, similarities
 import sys
 
 
-# 文件读取
-def readFile(path):
-    with open(path, 'r', encoding='utf-8') as file:
-        text = file.read()
-    return text
-
-
-# 从html中提取文本
-def extractionText(html):
-    soup = BeautifulSoup(html, 'html.parser')
-    td_tags = soup.find_all('td')
-    textList = []
-    # 提取文本内容并存储
-    for td in td_tags:
-        textList.append(td.text)
-
-    string = ''.join(textList)
-    return string
-
-
-# 判断文本是否html代码
-def isHtml(text):
-    soup = BeautifulSoup(text, 'html.parser')
-    td_tags = soup.find_all('td')
-    if (len(td_tags)):
-        return True
-    else:
-        return False
-
-
-# 内容清洗
-def Clean(arr):
-    result = []
-    for item in arr:
-        # 仅匹配中英文
-        if (re.match(u"[a-zA-Z0-9\u4e00-\u9fa5]", item)):
-            result.append(item)
-        else:
-            pass
-
-    return result
-
-
-# 分词
-def Participle(text):
-    seg_list = jieba.lcut(text)  # 默认是精确模式
-    return (seg_list)
-
-
-# 频率计算
-def CalcFrequency(text1List1, textList2):
-    textList = [text1List1, textList2]
-    # 映射到唯一的ID
-    dictionary = corpora.Dictionary(textList)
-    # 文本转化为词袋（bag-of-words）表示
-    corpus = [dictionary.doc2bow(text) for text in textList]
-    return dictionary, corpus
-
-
-# 余弦相似度计算
-def CalcSimilarity(text1, text2):
-    dictionary, corpus = CalcFrequency(text1, text2)
-    similarity = similarities.Similarity('-Similarity-index', corpus, num_features=len(dictionary))
-    test_corpus_1 = dictionary.doc2bow(text1)
-    # 计算余弦相似度
-    cos_similarity = similarity[test_corpus_1][1]
-    return cos_similarity
-
-
 # 获取命令行参数
-def GetArgs():
+def get_args():
     # 命令行参数列表
     args = sys.argv
 
@@ -86,8 +17,77 @@ def GetArgs():
         return '', ''
 
 
+# 文件读取
+def read_file(path):
+    with open(path, 'r', encoding='utf-8') as file:
+        text = file.read()
+    return text
+
+
+# 从html中提取文本
+def extraction_text(html):
+    soup = BeautifulSoup(html, 'html.parser')
+    td_tags = soup.find_all('td')
+    text_list = []
+    # 提取文本内容并存储
+    for td in td_tags:
+        text_list.append(td.text)
+
+    string = ''.join(text_list)
+    return string
+
+
+# 判断文本是否html代码
+def is_html(text):
+    soup = BeautifulSoup(text, 'html.parser')
+    td_tags = soup.find_all('td')
+    if len(td_tags):
+        return True
+    else:
+        return False
+
+
+# 分词
+def participle(text):
+    seg_list = jieba.lcut(text)  # 默认是精确模式
+    return seg_list
+
+
+# 内容清洗
+def clean(arr):
+    result = []
+    for item in arr:
+        # 仅匹配中英文
+        if re.match(u"[a-zA-Z0-9\u4e00-\u9fa5]", item):
+            result.append(item)
+        else:
+            pass
+
+    return result
+
+
+# 频率计算
+def calc_frequency(text1_list1, text_list2):
+    text_list = [text1_list1, text_list2]
+    # 映射到唯一的ID
+    dictionary = corpora.Dictionary(text_list)
+    # 文本转化为词袋（bag-of-words）表示
+    corpus = [dictionary.doc2bow(text) for text in text_list]
+    return dictionary, corpus
+
+
+# 余弦相似度计算
+def calc_similarity(text1, text2):
+    dictionary, corpus = calc_frequency(text1, text2)
+    similarity = similarities.Similarity('-Similarity-index', corpus, num_features=len(dictionary))
+    test_corpus_1 = dictionary.doc2bow(text1)
+    # 计算余弦相似度
+    cos_similarity = similarity[test_corpus_1][1]
+    return cos_similarity
+
+
 # 答案写入
-def writeAnswer(path, sim):
+def write_answer(path, sim):
     judge = '判定为抄袭' if sim > 0.6 else '不认定为抄袭'
     try:
         with open(path, 'w') as file:
@@ -101,35 +101,34 @@ def writeAnswer(path, sim):
 # 主函数
 def main():
     # 参数获取
-    path1, path2, path3 = GetArgs()
+    path1, path2, path3 = get_args()
     if path1 == '' or path2 == '' or path3 == '':
         return
 
-    original_text1 = readFile(path1)  # 原文
-    original_text2 = readFile(path2)  # html
+    original_text1 = read_file(path1)  # 原文
+    original_text2 = read_file(path2)  # html
 
     # 提取
-    if isHtml(original_text1):
-        text1_pro = extractionText(original_text1)
+    if is_html(original_text1):
+        text1_pro = extraction_text(original_text1)
     else:
         text1_pro = original_text1
 
-    if isHtml(original_text2):
-        text2_pro = extractionText(original_text2)
+    if is_html(original_text2):
+        text2_pro = extraction_text(original_text2)
     else:
         text2_pro = original_text2
 
-    # 分词
-    text1 = Clean(Participle(text1_pro))
-    text2 = Clean(Participle(text2_pro))
+    # 分词 清洗
+    text1 = clean(participle(text1_pro))
+    text2 = clean(participle(text2_pro))
 
     # 相似度 抄袭判定
-    sim = (CalcSimilarity(text1, text2))
+    sim = (calc_similarity(text1, text2))
 
     # 答案写入
-    writeAnswer(path3, sim)
+    write_answer(path3, sim)
 
-    print(sim)
 
 if __name__ == "__main__":
     main()
